5bb53077efa609ee9c53b93e6351e464cfecd1de
Aliaksandr Basau <abasau@plex.com>
2025-04-13 00:18:48 -0700

    GH pages

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   test: {
     environment: 'jsdom',
     globals: true
-  }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
 })
